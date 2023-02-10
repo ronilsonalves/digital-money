2cd8c7ca7c0461870f9043505889619adccaa7c0
Ronilson Alves <falecom@ronilsonalves.com>
2023-02-09 22:29:23 -0300

    📝 docs: configure spring-docs-ui to implements swagger-ui

---
+++ users-service/src/main/java/com/digitalhouse/money/usersservice/config/SwaggerConfig.java
@@ -0,0 +1,34 @@
+package com.digitalhouse.money.usersservice.config;
+
+import io.swagger.v3.oas.models.OpenAPI;
+import io.swagger.v3.oas.models.info.Contact;
+import io.swagger.v3.oas.models.info.Info;
+import io.swagger.v3.oas.models.info.License;
+import org.springdoc.core.models.GroupedOpenApi;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+
+@Configuration
+public class SwaggerConfig {
+
+    @Bean
+    public OpenAPI customOpenAPI() {
+        return new OpenAPI()
+                .info(new Info().title("Users API")
+                        .description("Users API for Digital Money")
+                        .version("0.0.1")
+                        .contact(new Contact().name("Digital Money")
+                                .email("hello@digitalhouse.com")
+                                .url("https://digitalhouse.com"))
+                        .license(new License().name("MIT License").url("https://digitalhouse.com")));
+    }
+
+    @Bean
+    public GroupedOpenApi usersAPI() {
+        return GroupedOpenApi.builder()
+                .group("Users")
+                .displayName("Users API")
+                .pathsToMatch("/users/**")
+                .build();
+    }
+}
