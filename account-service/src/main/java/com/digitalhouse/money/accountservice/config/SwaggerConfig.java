1cfea5009327b6c26e8e5230b2069456f010011c
Ronilson Alves <falecom@ronilsonalves.com>
2023-03-01 13:37:13 -0300

    🔒️ feat: implements resource server to microservice
    Was implemented resource server to secure the microservice, also implemented env vars in both microservices(accounts & users) they need to configure clients and secrets from Keycloak

--- account-service/src/main/java/com/digitalhouse/money/accountservice/config/SwaggerConfig.java
+++ account-service/src/main/java/com/digitalhouse/money/accountservice/config/SwaggerConfig.java
@@ -7,6 +7,7 @@ import io.swagger.v3.oas.models.OpenAPI;
 import io.swagger.v3.oas.models.info.Contact;
 import io.swagger.v3.oas.models.info.Info;
 import io.swagger.v3.oas.models.info.License;
+import org.springdoc.core.models.GroupedOpenApi;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 
@@ -17,13 +18,22 @@ public class SwaggerConfig {
     @Bean
     public OpenAPI customOpenAPI() {
         return new OpenAPI()
-                .info(new Info().title("Users API")
-                        .description("Users API for Digital Money")
+                .info(new Info().title("Account's API")
+                        .description("Account's API for Digital Money")
                         .version("0.0.1")
                         .contact(new Contact().name("Digital Money")
                                 .email("hello@digitalhouse.com")
                                 .url("https://digitalhouse.com"))
                         .license(new License().name("MIT License").url("https://digitalhouse.com")));
+    }
+
+    @Bean
+    public GroupedOpenApi accountsAPI() {
+        return GroupedOpenApi.builder()
+                .group("Accounts")
+                .displayName("Account's API")
+                .pathsToMatch("/api/accounts/**")
+                .build();
     }
 
 }
--- account-service/src/main/java/com/digitalhouse/money/accountservice/controller/AccountController.java
+++ account-service/src/main/java/com/digitalhouse/money/accountservice/controller/AccountController.java
@@ -4,8 +4,11 @@ import com.digitalhouse.money.accountservice.data.dto.AccountCreateRequestDTO;
 import com.digitalhouse.money.accountservice.data.model.Account;
 import com.digitalhouse.money.accountservice.exceptionhandler.ConflictException;
 import com.digitalhouse.money.accountservice.service.AccountService;
+import io.swagger.v3.oas.annotations.security.SecurityRequirement;
+import io.swagger.v3.oas.annotations.tags.Tag;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.HttpStatus;
+import org.springframework.security.access.prepost.PreAuthorize;
 import org.springframework.web.bind.annotation.*;
 
 import java.math.BigDecimal;
@@ -13,19 +16,21 @@ import java.util.UUID;
 
 @RestController
 @RequestMapping("/api/")
+@Tag(name = "Accounts")
+@SecurityRequirement(name = "BearerAuth")
 public class AccountController {
 
     @Autowired
     private AccountService service;
 
-    @PostMapping("account")
+    @PostMapping("accounts")
     @ResponseStatus(HttpStatus.CREATED)
     public Account create(@RequestBody AccountCreateRequestDTO dto) throws ConflictException {
         Account account = Account.builder().userId(dto.getUserId()).available_amount(new BigDecimal(0)).build();
         return service.save(account);
     }
 
-    @GetMapping("account/{account_id}")
+    @GetMapping("accounts/{account_id}")
     public  Account getAccountById(@RequestParam UUID account_id){
         return service.getById(account_id);
     }
