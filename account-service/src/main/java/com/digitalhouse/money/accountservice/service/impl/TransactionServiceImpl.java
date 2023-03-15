2fde53e332ebd68bfa97eb6188de462b812a17bd
Rodrigo Brasil <maic_rodrigo@hotmail.com>
2023-03-14 22:07:37 -0300

    feat: add get last five transactions

--- account-service/src/main/java/com/digitalhouse/money/accountservice/service/impl/TransactionServiceImpl.java
+++ account-service/src/main/java/com/digitalhouse/money/accountservice/service/impl/TransactionServiceImpl.java
@@ -15,11 +15,13 @@ import com.digitalhouse.money.accountservice.service.TransactionService;
 import com.digitalhouse.money.accountservice.util.VerifyAuthenticationUtil;
 import lombok.RequiredArgsConstructor;
 import org.springframework.beans.BeanUtils;
+import org.springframework.data.domain.Example;
 import org.springframework.data.domain.Page;
 import org.springframework.data.domain.Pageable;
 import org.springframework.stereotype.Service;
 
 import java.time.LocalDate;
+import java.util.ArrayList;
 import java.util.List;
 import java.util.Optional;
 import java.util.UUID;
@@ -43,24 +45,24 @@ public class TransactionServiceImpl implements TransactionService {
      * @return a Data Transfer Object for a transaction created successfully
      * @throws ResourceNotFoundException when accountID
      *                                   provided not exists.
-     * @throws UnauthorizedException when user try to access data impersonating another user account
+     * @throws UnauthorizedException     when user try to access data impersonating another user account
      */
     @Override
     public TransactionResponseDTO save(TransactionRequestDTO transactionData, UUID accountId) throws ResourceNotFoundException {
-        Account account = accountRepository.findById(accountId).orElseThrow(()->
+        Account account = accountRepository.findById(accountId).orElseThrow(() ->
                 new ResourceNotFoundException("There's no account with number provided."));
 
         if (!verifyAuthenticationUtil.isUserUUIDSameFromAuth(account.getUserId()))
             throw new UnauthorizedException("User not authorized");
 
-        if(transactionData.getTransactionType().equals(TransactionType.DEPÓSITO) && cardRepository.existsById(transactionData.getCardIdentification())) {
+        if (transactionData.getTransactionType().equals(TransactionType.DEPÓSITO) && cardRepository.existsById(transactionData.getCardIdentification())) {
             Transaction toSave = new Transaction();
             TransactionResponseDTO response = new TransactionResponseDTO();
             transactionData.setOriginAccountNumber(accountId);
             transactionData.setRecipientAccountNumber(accountId);
             transactionData.setTransactionDate(LocalDate.now());
-            BeanUtils.copyProperties(transactionData,toSave);
-            BeanUtils.copyProperties(repository.save(toSave),response);
+            BeanUtils.copyProperties(transactionData, toSave);
+            BeanUtils.copyProperties(repository.save(toSave), response);
             response.setCardEnding(
                     cardRepository.findById(transactionData.getCardIdentification())
                             .get().getNumber().toString().substring(12)
@@ -87,24 +89,24 @@ public class TransactionServiceImpl implements TransactionService {
      */
     @Override
     public TransferResponseDTO save(TransferRequestDTO transferData, UUID accountId) throws ResourceNotFoundException {
-        Account account = accountRepository.findById(accountId).orElseThrow(()-> new ResourceNotFoundException("There" +
+        Account account = accountRepository.findById(accountId).orElseThrow(() -> new ResourceNotFoundException("There" +
                 "'s no account registered with number provided."));
         Account accountRecipient =
-                accountRepository.findById(transferData.getRecipientAccountNumber()).orElseThrow(()->
+                accountRepository.findById(transferData.getRecipientAccountNumber()).orElseThrow(() ->
                         new ResourceNotFoundException("Invalid account destination. Please verify the destination " +
                                 "number and try again"));
 
-        if(!verifyAuthenticationUtil.isUserUUIDSameFromAuth(account.getUserId()))
+        if (!verifyAuthenticationUtil.isUserUUIDSameFromAuth(account.getUserId()))
             throw new UnauthorizedException("User not authorized");
 
-        if(transferData.getTransactionType().equals(TransactionType.TRANSFERÊNCIA)) {
+        if (transferData.getTransactionType().equals(TransactionType.TRANSFERÊNCIA)) {
             Transaction toSave = new Transaction();
             TransferResponseDTO response = new TransferResponseDTO();
             if (transferData.getTransactionAmount().compareTo(account.getAvailable_amount()) >= 0)
                 throw new InsufficientFundsException("Unable to complete transfer. Account with insufficient funds to" +
                         " complete the transaction");
-            BeanUtils.copyProperties(transferData,toSave);
-            BeanUtils.copyProperties(repository.save(toSave),response);
+            BeanUtils.copyProperties(transferData, toSave);
+            BeanUtils.copyProperties(repository.save(toSave), response);
             //setting new values to available amount to accounts in transaction
             account.setAvailable_amount(account.getAvailable_amount().subtract(response.getTransactionAmount()));
             accountRecipient.setAvailable_amount(accountRecipient.getAvailable_amount().add(response.getTransactionAmount()));
@@ -129,13 +131,14 @@ public class TransactionServiceImpl implements TransactionService {
 
     /**
      * Returns a page of objects containing ITransferResponseDTO
-     * @param origin is the origin account number
-     * @param type of transaction. Default is TransactionType.TRANSFERÊNCIA
+     *
+     * @param origin   is the origin account number
+     * @param type     of transaction. Default is TransactionType.TRANSFERÊNCIA
      * @param pageable an object to interact with JPA Repository to mount the pageable return
      * @return Returns a page of objects containing ITransferResponseDTO
-     * @throws BadRequestException when the type of transactions is different from TransactionType.TRANSFERÊNCIA
+     * @throws BadRequestException       when the type of transactions is different from TransactionType.TRANSFERÊNCIA
      * @throws ResourceNotFoundException when the origin account number is invalid or non-existent.
-     * @throws UnauthorizedException when user try to fetch data from another account that's not his.
+     * @throws UnauthorizedException     when user try to fetch data from another account that's not his.
      */
     @Override
     public Page<ITransferResponseDTO> listAllByAccountOriginAndTypeOrdByDate(UUID origin,
@@ -148,18 +151,27 @@ public class TransactionServiceImpl implements TransactionService {
             throw new BadRequestException("Unfortunately you can't make this consult though this endpoint. To consult " +
                     " your account transactions activities use transactions resource."); //ToDo: add filter to
         //ToDo: transactions controller limiting up to 5 transactions as activities' endpoint
-        return repository.findAllByOriginAccountNumberAndTransactionTypeOrderByTransactionDate(origin,type,pageable);
+        return repository.findAllByOriginAccountNumberAndTransactionTypeOrderByTransactionDate(origin, type, pageable);
+    }
+
+    @Override
+    public List<TransactionResponseDTO> listLastFiveTransactionsByAccount(UUID accountId) throws ResourceNotFoundException {
+        Example<Transaction> example = Example.of(Transaction.builder().originAccountNumber(accountId).recipientAccountNumber(accountId).build());
+        Pageable page = Pageable.ofSize(5);
+        Page<Transaction> all = repository.findAll(example, page);
+        return all.map(this::createResponse).toList();
     }
 
 
     /**
      * Parses a transaction into a response object
+     *
      * @param transaction is a transaction object to be mapped into a DTO
      * @return TransactionResponseDTO.
      */
     private TransactionResponseDTO createResponse(Transaction transaction) {
         TransactionResponseDTO responseDTO = new TransactionResponseDTO();
-        BeanUtils.copyProperties(transaction,responseDTO);
+        BeanUtils.copyProperties(transaction, responseDTO);
         if (transaction.getTransactionType().equals(TransactionType.DEPÓSITO))
             responseDTO.setCardEnding(
                     cardRepository.findById(transaction.getCardIdentification())
