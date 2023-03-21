25d9fb1ffbcfee812fa65ebb0f53720f39f2b992
Rener Almeida <rener.almeida@gmail.com>
2023-03-21 22:10:05 +0000

    Api testing

--- APITesting/src/test/java/com/APITesting/APITesting/AccountDataVisualizationTest.java
+++ APITesting/src/test/java/com/APITesting/APITesting/AccountDataVisualizationTest.java
@@ -1,77 +1,82 @@
 package com.APITesting.APITesting;
 
 import io.restassured.RestAssured;
+import io.restassured.builder.RequestSpecBuilder;
 import io.restassured.response.Response;
 import io.restassured.specification.RequestSpecification;
 import org.junit.Assert;
-import org.junit.Test;
 import org.junit.jupiter.api.Tag;
+import org.junit.jupiter.api.Test;
 
-import static io.restassured.RestAssured.given;
-
-public class LogoutTest {
+public class AccountDataVisualizationTest {
 
     @Test
-    @Tag("Spring 1")
-    public void LogoutTest_validTokenShouldReturnCode200() {
-//        RestAssured.baseURI = "http://localhost:8081";
+    @Tag("Sprint 2")
+    public void CorrectId_shouldReturnCode200AndDetailedAccountData() {
         RestAssured.baseURI = "http://18.231.109.51:8081";
-        RequestSpecification request = given();
+        RequestSpecification request = RestAssured.given();
         request.header("Content-Type", "application/json");
         String jsonBody = "{"
-                + "\"email\": \"johndoe@gmail.com\","
+                + "\"email\": \"johnmc@gmail.com\","
                 + "\"password\": \"password123\""
                 + "}";
         Response response = request.body(jsonBody).post("/auth/login");
         String bearerToken = response.getBody().jsonPath().getString("token");
         String accessToken = bearerToken.replace("Bearer ", "");
-        request.header("Authorization", "Bearer " + accessToken);
-        response = request.post("auth/logout");
+        String account_id = "f81f973b-a3b1-47c0-a3cf-1b77d4bc2ee1";
+        RestAssured.baseURI = "http://18.231.109.51:8082";
+        RequestSpecification request2 = RestAssured.given();
+        request2.header("Authorization", "Bearer " + accessToken);
+        response = request2.get("/api/accounts/" + account_id);
         int statusCode = response.getStatusCode();
         Assert.assertEquals(statusCode, 200);
-    }
-
-    @Test
-    @Tag("Spring 1")
-    public void LogoutTest_invalidTokenShouldReturnCode401AndProperMessage() {
-//        RestAssured.baseURI = "http://localhost:8081";
-        RestAssured.baseURI = "http://18.231.109.51:8081";
-        RequestSpecification request = given();
-        request.header("Content-Type", "application/json");
-        String jsonBody = "{"
-                + "\"email\": \"johndoe@gmail.com\","
-                + "\"password\": \"password123\""
-                + "}";
-        Response response = request.body(jsonBody).post("/auth/login");
-        String bearerToken = response.getBody().jsonPath().getString("token");
-        String accessToken = bearerToken.replace("Bearer ", "xxxxxxxx");
-        request.header("Authorization", "Bearer " + accessToken);
-        response = request.post("auth/logout");
-        int statusCode = response.getStatusCode();
-        Assert.assertEquals(statusCode, 401);
         System.out.println("Response body: " + response.body().asString());
     }
 
     @Test
-    @Tag("Spring 1")
-    public void LogoutTest_expiredTokenShouldReturnCode401AndProperMessage() throws Exception {
-//        RestAssured.baseURI = "http://localhost:8081";
+    @Tag("Sprint 2")
+    public void InorrectId_shouldReturnCode404AndProperMessage() {
         RestAssured.baseURI = "http://18.231.109.51:8081";
-        RequestSpecification request = given();
+        RequestSpecification request = RestAssured.given();
         request.header("Content-Type", "application/json");
         String jsonBody = "{"
                 + "\"email\": \"johndoe@gmail.com\","
                 + "\"password\": \"password123\""
                 + "}";
         Response response = request.body(jsonBody).post("/auth/login");
-        Thread.sleep(301000);
         String bearerToken = response.getBody().jsonPath().getString("token");
         String accessToken = bearerToken.replace("Bearer ", "");
-        request.header("Authorization", "Bearer " + accessToken);
-        response = request.post("auth/logout");
+        String account_id = "a0ccd122-5159-4435-9430-d81ec53f7059";
+        RestAssured.baseURI = "http://18.231.109.51:8082";
+        RequestSpecification request2 = RestAssured.given();
+        request2.header("Authorization", "Bearer " + accessToken);
+        response = request2.get("/api/accounts/" + account_id);
+        int statusCode = response.getStatusCode();
+        Assert.assertEquals(statusCode, 404);
+        System.out.println("Response body: " + response.body().asString());
+    }
+
+    //Falhando, em vias de correção.
+    @Test
+    @Tag("Sprint 2")
+    public void AnotherUserId_shouldReturnCode401AndProperMessage() {
+        RestAssured.baseURI = "http://18.231.109.51:8081";
+        RequestSpecification request = RestAssured.given();
+        request.header("Content-Type", "application/json");
+        String jsonBody = "{"
+                + "\"email\": \"johndoe@gmail.com\","
+                + "\"password\": \"password123\""
+                + "}";
+        Response response = request.body(jsonBody).post("/auth/login");
+        String bearerToken = response.getBody().jsonPath().getString("token");
+        String accessToken = bearerToken.replace("Bearer ", "");
+        String account_id = "1d7cc85c-96c8-4cbf-95ff-2a5a3ff30ba7";
+        RestAssured.baseURI = "http://18.231.109.51:8082";
+        RequestSpecification request2 = RestAssured.given();
+        request2.header("Authorization", "Bearer " + accessToken);
+        response = request2.get("/api/accounts/" + account_id);
         int statusCode = response.getStatusCode();
         Assert.assertEquals(statusCode, 401);
         System.out.println("Response body: " + response.body().asString());
     }
-
 }
--- APITesting/src/test/java/com/APITesting/APITesting/TransactionsVisualizationTest.java
+++ APITesting/src/test/java/com/APITesting/APITesting/TransactionsVisualizationTest.java
@@ -4,19 +4,16 @@ import io.restassured.RestAssured;
 import io.restassured.response.Response;
 import io.restassured.specification.RequestSpecification;
 import org.junit.Assert;
-import org.junit.Test;
 import org.junit.jupiter.api.Tag;
+import org.junit.jupiter.api.Test;
 
-import static io.restassured.RestAssured.given;
-
-public class LogoutTest {
+public class TransactionsVisualizationTest {
 
     @Test
-    @Tag("Spring 1")
-    public void LogoutTest_validTokenShouldReturnCode200() {
-//        RestAssured.baseURI = "http://localhost:8081";
+    @Tag("Sprint 2")
+    public void CorrectAccountId_shouldReturnCode200AndDetailedTransactionData() {
         RestAssured.baseURI = "http://18.231.109.51:8081";
-        RequestSpecification request = given();
+        RequestSpecification request = RestAssured.given();
         request.header("Content-Type", "application/json");
         String jsonBody = "{"
                 + "\"email\": \"johndoe@gmail.com\","
@@ -25,53 +22,61 @@ public class LogoutTest {
         Response response = request.body(jsonBody).post("/auth/login");
         String bearerToken = response.getBody().jsonPath().getString("token");
         String accessToken = bearerToken.replace("Bearer ", "");
-        request.header("Authorization", "Bearer " + accessToken);
-        response = request.post("auth/logout");
+        String account_id = "a8ccd122-5159-4435-9430-d81ec53f7089";
+        RestAssured.baseURI = "http://18.231.109.51:8082";
+        RequestSpecification request2 = RestAssured.given();
+        request2.header("Authorization", "Bearer " + accessToken);
+        response = request2.get("/api/accounts/" + account_id + "/transactions");
         int statusCode = response.getStatusCode();
         Assert.assertEquals(statusCode, 200);
-    }
-
-    @Test
-    @Tag("Spring 1")
-    public void LogoutTest_invalidTokenShouldReturnCode401AndProperMessage() {
-//        RestAssured.baseURI = "http://localhost:8081";
-        RestAssured.baseURI = "http://18.231.109.51:8081";
-        RequestSpecification request = given();
-        request.header("Content-Type", "application/json");
-        String jsonBody = "{"
-                + "\"email\": \"johndoe@gmail.com\","
-                + "\"password\": \"password123\""
-                + "}";
-        Response response = request.body(jsonBody).post("/auth/login");
-        String bearerToken = response.getBody().jsonPath().getString("token");
-        String accessToken = bearerToken.replace("Bearer ", "xxxxxxxx");
-        request.header("Authorization", "Bearer " + accessToken);
-        response = request.post("auth/logout");
-        int statusCode = response.getStatusCode();
-        Assert.assertEquals(statusCode, 401);
         System.out.println("Response body: " + response.body().asString());
     }
 
+    //Falhando, em vias de correção.
     @Test
-    @Tag("Spring 1")
-    public void LogoutTest_expiredTokenShouldReturnCode401AndProperMessage() throws Exception {
-//        RestAssured.baseURI = "http://localhost:8081";
+    @Tag("Sprint 2")
+    public void IncorrectAccountId_shouldReturnCode404AndProperMessage() {
         RestAssured.baseURI = "http://18.231.109.51:8081";
-        RequestSpecification request = given();
+        RequestSpecification request = RestAssured.given();
         request.header("Content-Type", "application/json");
         String jsonBody = "{"
                 + "\"email\": \"johndoe@gmail.com\","
                 + "\"password\": \"password123\""
                 + "}";
         Response response = request.body(jsonBody).post("/auth/login");
-        Thread.sleep(301000);
         String bearerToken = response.getBody().jsonPath().getString("token");
         String accessToken = bearerToken.replace("Bearer ", "");
-        request.header("Authorization", "Bearer " + accessToken);
-        response = request.post("auth/logout");
+        String account_id = "c7ccd122-5159-4435-9430-d81ec53f7078";
+        RestAssured.baseURI = "http://18.231.109.51:8082";
+        RequestSpecification request2 = RestAssured.given();
+        request2.header("Authorization", "Bearer " + accessToken);
+        response = request2.get("/api/accounts/" + account_id + "/transactions");
+        int statusCode = response.getStatusCode();
+        Assert.assertEquals(statusCode, 404);
+        System.out.println("Response body: " + response.body().asString());
+    }
+
+    //Falhando, em vias de correção.
+    @Test
+    @Tag("Sprint 2")
+    public void DifferentUserId_shouldReturnCode401AndProperMessage() {
+        RestAssured.baseURI = "http://18.231.109.51:8081";
+        RequestSpecification request = RestAssured.given();
+        request.header("Content-Type", "application/json");
+        String jsonBody = "{"
+                + "\"email\": \"johndoe@gmail.com\","
+                + "\"password\": \"password123\""
+                + "}";
+        Response response = request.body(jsonBody).post("/auth/login");
+        String bearerToken = response.getBody().jsonPath().getString("token");
+        String accessToken = bearerToken.replace("Bearer ", "");
+        String account_id = "1d7cc85c-96c8-4cbf-95ff-2a5a3ff30ba7";
+        RestAssured.baseURI = "http://18.231.109.51:8082";
+        RequestSpecification request2 = RestAssured.given();
+        request2.header("Authorization", "Bearer " + accessToken);
+        response = request2.get("/api/accounts/" + account_id + "/transactions");
         int statusCode = response.getStatusCode();
         Assert.assertEquals(statusCode, 401);
         System.out.println("Response body: " + response.body().asString());
     }
-
 }
